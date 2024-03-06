ea2afb1f04ec30dff7060665abbf1be1a750fbde
meigston <meigston@live.com>
2024-03-06 08:03:18 -0300

    added more options

--- Program.cs
+++ Program.cs
@@ -1,28 +1,105 @@
 ﻿using Consul;
+using System.Text.Json;
 
-var consulClient = new ConsulClient(c =>
+//ConsulClient consulClient = CreateConsulClient();
+while (true)
 {
-    Console.WriteLine("Put your Consul URL");
-    c.Address = new Uri(Console.ReadLine() ?? "http://localhost:8500");
-    Console.WriteLine("Put your Consul TOKEN");
+    var consulClient = CreateConsulClient();
+    await MenuOption(consulClient);
+}
 
-    // Substitua "seu-token-aqui" pelo seu token de acesso real
-    c.Token = Console.ReadLine(); //"FBAF54CC-E03D-4763-9F19-376114D3857B";
-});
 
-// Substitua "nomeInicial" pelo prefix desejado
-Console.WriteLine("Digite o prefixo que deseja clonar: ");
-var prefix = Console.ReadLine();
-var keysEValues = await FindKeys(consulClient, prefix);
-
-Console.WriteLine("Digite o NOVO prefixo onde deseja criar o clone: ");
-var newPrefix = Console.ReadLine();
-
-foreach (var par in keysEValues)
+static async Task MenuOption(ConsulClient consulClient)
 {
-    Console.WriteLine($"Chave: {par.Key}, Valor: {par.Value}");
-    // Substitua "novoPrefixo" pelo prefix onde você deseja criar o clone das chaves
-    await CreateKey(consulClient, $"{newPrefix}/{par.Key}", par.Value);
+    Console.WriteLine($"Choose an option:{Environment.NewLine} 1) Export {Environment.NewLine} 2) Import {Environment.NewLine} 3) Clone");
+    var opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Enter the settings prefix to export: ");
+            var prefixToExport = Console.ReadLine();
+
+            Console.WriteLine("Enter the file path to save the settings: ");
+            var exportFilePath = Console.ReadLine();
+
+            await ExportConfigurations(consulClient, prefixToExport, exportFilePath);
+
+            Console.Clear();
+            Console.WriteLine($"Settings exported successfully. {Environment.NewLine}");
+            break;
+        case "2":
+            Console.WriteLine("Enter the file path of the settings to import: ");
+            var importFilePath = Console.ReadLine();
+
+            Console.WriteLine("Enter the new prefix for the imported settings: ");
+            var newPrefixForImport = Console.ReadLine();
+
+            await ImportConfigurations(consulClient, importFilePath, newPrefixForImport);
+
+            Console.Clear();
+            Console.WriteLine($"Settings imported successfully. {Environment.NewLine}");
+            break;
+        case "3":
+            await CloneStructure(consulClient);
+            Console.Clear();
+            Console.WriteLine($"Settings cloned successfully. {Environment.NewLine}");
+            break;
+        //case "4":
+        //    initConsul = true;
+        //    break;
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+}
+
+
+static ConsulClient CreateConsulClient()
+{
+    return new ConsulClient(c =>
+    {
+        Console.WriteLine("Put your Consul URL");
+        c.Address = new Uri(Console.ReadLine() ?? "http://localhost:8500");
+
+        Console.WriteLine("Put your Consul TOKEN");
+        c.Token = Console.ReadLine();
+    });
+}
+
+static async Task CloneStructure(ConsulClient consulClient)
+{
+    Console.WriteLine("Enter the prefix you want to clone: ");
+    var prefix = Console.ReadLine();
+    var keysEValues = await FindKeys(consulClient, prefix);
+
+    Console.WriteLine("Enter the NEW prefix where you want to create the clone: ");
+    var newPrefix = Console.ReadLine();
+
+    foreach (var par in keysEValues)
+    {
+        Console.WriteLine($"Key: {par.Key}, Value: {par.Value}");
+        await CreateKey(consulClient, $"{newPrefix}/{par.Key}", par.Value);
+    }
+}
+
+static async Task ExportConfigurations(ConsulClient consulClient, string prefix, string filePath)
+{
+    var keysEValues = await FindKeys(consulClient, prefix);
+    var json = System.Text.Json.JsonSerializer.Serialize(keysEValues, new JsonSerializerOptions { WriteIndented = true });
+    await System.IO.File.WriteAllTextAsync(filePath, json);
+}
+
+static async Task ImportConfigurations(ConsulClient consulClient, string filePath, string newPrefix)
+{
+    var json = await System.IO.File.ReadAllTextAsync(filePath);
+    var keysEValues = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+
+    foreach (var par in keysEValues)
+    {
+        Console.WriteLine($"Key: {par.Key}, Value: {par.Value}");
+        await CreateKey(consulClient, $"{newPrefix}/{par.Key}", par.Value);
+    }
 }
 
 
